f794e3bb2d94b844efc2ef001daf9c3e2c505b16
jagadhis <jwaran78@gmail.com>
2024-09-03 23:21:22 +0530

    fix: clean codebase

--- debug.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-echo "Listing /opt/pinot/data:"
-ls -l /opt/pinot/data
-echo "Content of job-spec-json.yaml:"
-cat /opt/pinot/data/job-spec-json.yaml
-echo "Attempting to read schema file:"
-cat /opt/pinot/data/electric_vehicles_schema.json
